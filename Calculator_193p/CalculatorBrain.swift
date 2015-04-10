0ba3a6afc8ca46a10373f1dced137dae4699f63e
Shane Hughes <shughes@student.yosemite.edu>
2015-04-10 08:06:01 -0700

    auto layout stuff

---
+++ Calculator_193p/CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorBrain.swift
+//  Calculator_193p
+//
+//  Created by Worship Arts on 4/10/15.
+//  Copyright (c) 2015 Shane Hughes. All rights reserved.
+//
+
+import Foundation
--- Calculator_193p/ViewController.swift
+++ Calculator_193p/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    //The colon in the declaration means “…of type…,” so the code above can be read as:
+    //“Declare a variable called display that is of type Boolean.”
+    
+    //green arrow that goes from the controller to the model
+    //var brain = CalculatorBrain()
+
+    @IBAction func appendDigit(sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
